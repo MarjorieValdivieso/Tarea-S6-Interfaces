89294ec9ed9c5aff91b3e454bf50e0b7400a7aba
MarjorieValdivieso <marjorievillacis430@gmail.com>
2026-05-12 17:01:01 -0500

    clase HorarioAcademico

--- Ejercicio1/src/HorarioAcademico.java
+++ Ejercicio1/src/HorarioAcademico.java
@@ -1,2 +1,13 @@
-public class HorarioAcademico {
+import javax.swing.plaf.PanelUI;
+
+public class HorarioAcademico implements Imprimible{
+   private String periodoAcademico;
+   public HorarioAcademico(String periodoAcademico){
+       this.periodoAcademico=periodoAcademico;
+   }
+   @Override
+    public void imprimir(){
+       System.out.println("Imprimiendo horario academico");
+       System.out.println("Periodo academico: "+periodoAcademico);
+   }
 }
