5c814f51732400025e0cf2afa1173fd32da3f8b7
MarjorieValdivieso <marjorievillacis430@gmail.com>
2026-05-12 17:03:14 -0500

    clase ActaNotas

--- Ejercicio1/src/ActaNotas.java
+++ Ejercicio1/src/ActaNotas.java
@@ -1,2 +1,11 @@
-public class ActaNotas {
+public class ActaNotas implements Imprimible{
+    private String carrera;
+    public void ActaNotas(String carrera){
+        this.carrera=carrera;
+    }
+    @Override
+    public void imprimir(){
+        System.out.println("Imprimiendo acta de notas");
+        System.out.println("Carrera: "+carrera);
+    }
 }
